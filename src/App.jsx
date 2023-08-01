4e2f766a34df1f805c1a2c0d9ddc584a028ac0d2
Nik0Sp <nikopringer@gmail.com>
2023-08-01 23:08:56 +0200

    images update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-
+import Picture1 from "./images/picture1.png";
+import Picture2 from "./images/picture2.png";
+import Picture3 from "./images/picture3.png";
+import Picture4 from "./images/picture4.png";
 function App() {
+  
   return (
-    <div className="App">
-     hm
+    <div className="section">
+      <h1 className="title">Simple Image Slider</h1>
+      <div className="container">
+        <div className="image-container">
+          <img src={Picture1} alt="" />
+          <img src={Picture2} alt="" />
+          <img src={Picture3} alt="" />
+          <img src={Picture4} alt="" />
+        </div>
+        <div className="dot-container">
+          <button onClick="dot(1)"></button>
+          <button onClick="dot(2)"></button>
+          <button onClick="dot(3)"></button>
+          <button onClick="dot(4)"></button>
+        </div>
+        <button id="prev" onClick="prev()">
+          prev
+        </button>
+        <button id="back" onClick="back()">
+          back
+        </button>
+      </div>
     </div>
   );
 }
