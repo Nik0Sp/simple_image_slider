35004d9b6fc23f8e38bc256837e31433320c909a
Nik0Sp <nikopringer@gmail.com>
2023-08-03 00:19:07 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
+import React, { useState } from "react";
 import Picture1 from "./images/picture1.png";
 import Picture2 from "./images/picture2.png";
 import Picture3 from "./images/picture3.png";
 import Picture4 from "./images/picture4.png";
+
 function App() {
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  const images = [Picture1, Picture2, Picture3, Picture4];
+
+  const next = () => {
+    setActiveImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const back = () => {
+    setActiveImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+
+
   return (
     <div className="section">
       <h1 className="title">Simple Image Slider</h1>
       <div className="container">
         <div className="image-container">
-          <img src={Picture1} alt="" />
-          <img src={Picture2} alt="" />
-          <img src={Picture3} alt="" />
-          <img src={Picture4} alt="" />
+          {images.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt=""
+              className={index === activeImageIndex ? "active" : ""}
+            />
+          ))}
         </div>
-        <div className="dot-container">
-          <button onClick="dot(1)"></button>
-          <button onClick="dot(2)"></button>
-          <button onClick="dot(3)"></button>
-          <button onClick="dot(4)"></button>
-        </div>
-        <button id="back" onClick="back()">
+
+        <button id="back" onClick={back}>
           back
         </button>
-        <button id="next" onClick="next()">
+        <button id="next" onClick={next}>
           next
         </button>
       </div>
